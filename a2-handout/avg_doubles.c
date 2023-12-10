fae940a9e16161ede568df50cc100af523a9040a
Nodifar <113943830+Nodifar@users.noreply.github.com>
2023-12-10 18:32:10 +0100

    Update avg_doubles.c

--- a2-handout/avg_doubles.c
+++ a2-handout/avg_doubles.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Assuming read_double_bin() and write_double_ascii() are defined elsewhere
 extern double read_double_bin();
 extern void write_double_ascii(double);
 
@@ -11,20 +10,17 @@ int main() {
     double sum = 0.0;
     int count = 0;
     double number;
-
-    // Read numbers until EOF
+    
     while (fread(&number, sizeof(double), 1, stdin)) {
         sum += number;
         count++;
     }
 
-    // Check if no numbers were read
     if (count == 0) {
         fprintf(stderr, "No numbers input.\n");
         return 1;
     }
 
-    // Compute and print the average
     double avg = sum / count;
     write_double_ascii(avg);
     printf("\n");
